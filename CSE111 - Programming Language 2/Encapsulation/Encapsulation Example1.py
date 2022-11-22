178d15c4ec76d06d36e10bd26d7c923b4c94b17d
MD. Fakhruddin Gazzali Fahim <fahim9778@gmail.com>
2022-11-22 04:30:07 +0000

    added example to Encapsulation

--- CSE111 - Programming Language 2/Encapsulation/Encapsulation Example1.py
+++ CSE111 - Programming Language 2/Encapsulation/Encapsulation Example1.py
@@ -3,7 +3,14 @@ class Employee:
         self.name = name
         self.age = age
         self.email = email
-        self.__salary = 10000
+        self.__salary = 10000 ##PRIVATE VARIABLE
+
+    def get_salary(self):
+        return self.__salary
+    
+    def set_salary(self, Newsalary):
+        self.__salary = Newsalary
+
 
     def printDetails(self):
         print(f"Name: {self.name}, Age: {self.age}, Email: {self.email}, Salary: {self.__salary}")
@@ -21,4 +28,6 @@ emp1.__salary = 100
 emp1.printDetails()
 print(emp1.__salary)
 
-print(emp1.address)
+print(emp1.get_salary())
+emp1.set_salary(100)
+emp1.printDetails()
