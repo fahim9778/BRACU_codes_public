5e644b8aec1b37e713a1f30bbf4f849809a3101d
MD. Fakhruddin Gazzali Fahim <fahim9778@gmail.com>
2022-12-09 08:31:14 +0000

    added CSE111 Fall22 Quiz3 Solution

--- CSE111 - Programming Language 2/Question Solve/CSE111_Quiz_3_sec_25_26_Fall22_FGZ.py
+++ CSE111 - Programming Language 2/Question Solve/CSE111_Quiz_3_sec_25_26_Fall22_FGZ.py
@@ -1,15 +1,19 @@
 # Create a python class "Clock" that shows examples of Operator Overloading.
 
 class Clock:
-    def __init__(self, time):
+    def __init__(self, time : str):
         self.hour, self.min = int(time.split(':')[0]), int(time.split(':')[1])
         if (self.hour < 0 or self.hour > 23) or (self.min < 0 or self.min > 59):
             self.hour, self.min = None, None
-            raise ValueError('Invalid time')
+            raise ValueError('Invalid time') # rasie is used to raise an exception explicitly.
 
     def __add__(self, other):
         if isinstance(other, int):
             return Clock(f'{(self.hour + other) % 24}:{self.min}')
+        if isinstance(other, str):
+            return self + Clock(other)
+            # new_hour, new_min = divmod(self.min + int(other.split(':')[1]), 60)
+            # return Clock(f'{(new_hour + self.hour + int(other.split(":")[0])) % 24}:{new_min}')
         elif isinstance(other, Clock):
             # new_hour, new_min = divmod(self.min + other.min, 60)
             new_hour = (self.min + other.min) // 60
@@ -21,8 +25,16 @@ class Clock:
     def __sub__(self, other):
         if isinstance(other, int):
             return Clock(f'{(self.hour - other) % 24}:{self.min}')
+        if isinstance(other, str):
+            return self - Clock(other)
+            # new_hour, new_min = divmod(self.min - int(other.split(':')[1]), 60)
+            # return Clock(f'{(new_hour + self.hour - int(other.split(":")[0])) % 24}:{new_min}')
+
         elif isinstance(other, Clock):
             # new_hour, new_min = divmod(self.min - other.min, 60)
+            # divmod() returns a tuple of (quotient, remainder)
+            # For example, divmod(5, 2) returns (2, 1)
+            # where 2 is the quotient and 1 is the remainder if we divide 5 by 2.
             new_hour = (self.min - other.min) // 60
             new_min = (self.min - other.min) % 60
             return Clock(f'{(new_hour + self.hour - other.hour) % 24}:{new_min}')
@@ -40,10 +52,12 @@ class Clock:
     __rsub__ = __sub__
 
 
+time_0 = Clock('2:00')
 time_1 = Clock('10:30')
 time_2 = Clock('19:45')
 time_3 = Clock('16:16')
 
+print(time_0 + time_1)
 print(time_1 + time_2)
 print(time_2 - time_1)
 print(time_2 - 10)
@@ -54,3 +68,5 @@ print(time_1 - time_2 + time_3)
 
 # time_4 = Clock('25:30')
 # time_5 = Clock('10:70')
+
+print(time_1 + '10:50')
